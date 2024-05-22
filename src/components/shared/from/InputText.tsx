c33888717b5e0d5f82d0eb25349b346c84c06952
tufaelkhan <tufaelkhan247@gmail.com>
2024-05-23 02:05:02 +0600

    add redirects page

--- src/components/shared/from/InputText.tsx
+++ src/components/shared/from/InputText.tsx
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Controller, useFormContext } from "react-hook-form";
-
 type TInputField = {
-  type: string;
+  type?: string;
   name: string;
   label: string;
 };
 
-export default function InputText({ type, name, label }: TInputField) {
+export default function InputText({ type = "text", name, label }: TInputField) {
   const {
     control,
     formState: { errors },
@@ -14,27 +14,30 @@ export default function InputText({ type, name, label }: TInputField) {
 
   return (
     <Controller
-      name={name}
-      control={control}
-      defaultValue=""
-      render={({ field }) => (
-        <div className=" w-full">
-          <label htmlFor={name} className=" block">
-            {label}
-          </label>
-          <input
-            {...field}
-            id={name}
-            placeholder={label}
-            type={type}
-            className="input input-bordered w-full mt-2"
-          />
+    name={name}
+    control={control}
+    defaultValue=""
+    render={({ field: { onChange, onBlur, value, ref } }) => (
+      <div className="w-full">
+        <label htmlFor={name} className="block">
+          {label}
+        </label>
+        <input
+          onChange={onChange}
+          onBlur={onBlur}
+          value={value}
+          ref={ref}
+          id={name}
+          placeholder={label}
+          type={type}
+          className="input input-bordered w-full mt-2"
+        />
 
-          {errors[name] && (
-            <p className="text-error">{errors[name]?.message as string}</p>
-          )}
-        </div>
-      )}
-    />
+        {errors[name] && (
+          <p className="text-error">{errors[name]?.message as string}</p>
+        )}
+      </div>
+    )}
+  />
   );
 }
